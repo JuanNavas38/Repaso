348db4463caae46f3c4fde128b5feaedd3c14019
Juan Navas <75100643+JuanNavas38@users.noreply.github.com>
2025-06-17 21:21:15 -0500

    Estructura inicial del proyecto: src, notebooks, data, docs

---
+++ src/leccion4.py
@@ -0,0 +1,12 @@
+# Programa que calcula e imprime en pantalla la suma de dos números
+
+# Crear las variables
+a = 5
+b = 4
+
+# Calcular la suma
+suma = a + b
+
+# Imprimir resultado en pantalla
+print("Resultado de la suma:")
+print(suma)
